beafcb586814d2cc2aacf0e9c813897cacfdacae
jun <doubleat.hj@gmail.com>
2020-08-08 20:55:25 +0900

    add insertionSort.js

--- src/bubbleSort.js
+++ src/bubbleSort.js
@@ -36,3 +36,18 @@ const bubbleSort = function (array) {
   }
   return array;
 };
+
+//insertion sort
+const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i++) {
+    let temp = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && array[j] > temp) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = temp;
+  }
+  return array;
+};
